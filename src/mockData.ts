7883e78bd3dbed03a0c4263cff34a708d7810bca
helenwijaya95 <helen.wijaya95@gmail.com>
2026-04-16 00:05:43 +0800

    update mock-data

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,10 +1,12 @@
 // src/constants/mockData.ts
 export const MOCK_REFACTOR_RESULT = [
   {
+    "originalClassName":".card",
     "tailwindClasses": "flex flex-col items-center p-4 m-2 mx-auto bg-white border border-gray-200 rounded-lg max-w-sm hover:shadow-lg",
     "explanation": "Mock container styles"
   },
   {
+    "originalClassName":".card-title",
     "tailwindClasses": "text-xl font-bold text-gray-900 mb-2",
     "explanation": "Mock title styles"
   }
